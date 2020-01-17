8e1563b4d88a0f8c85f15b158425018cf7ab57fd
Tenjyo <hykw77@gmail.com>
2020-01-17 16:36:15 +0900

    first commit

---
+++ sample/ModusPonens.v
@@ -0,0 +1,42 @@
+From mathcomp
+  Require Import ssreflect.
+
+Section ModusPonens.
+Variables X Y :Prop.
+
+Hypothesis XtoY_is_true : X -> Y.
+Hypothesis X_is_true : X.
+
+Theorem MP : Y.
+move: X_is_true.
+by [].
+Qed.
+
+End ModusPonens.
+
+
+Section HilbertSAxiom.
+Variables A B C : Prop.
+
+Theorem HS1: (A -> (B -> C)) ->  ((A -> B) -> (A -> C)).
+Proof.
+move=> AtoBtoC_is_true.
+move=> AtoB_is_true.
+move=> A_is_true.
+
+apply: (MP B C).
+
+apply: (MP A (B -> C)).
+by [].
+by [].
+
+apply: (MP A B).
+by [].
+by [].
+
+Qed.
+
+End HilbertSAxiom.
+
+
+
